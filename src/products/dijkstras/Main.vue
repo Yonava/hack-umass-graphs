0a2749a7d31b97cf0081caf9c2e16ce146f33a42
yonava <yona@vossandreae.com>
2024-11-09 09:24:13 -0500

    added starter code for each product

---
+++ src/products/dijkstras/Main.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+  import { ref } from "vue";
+  import { useGraph } from "@graph/useGraph";
+  import Graph from "@graph/Graph.vue";
+
+  const graphEl = ref<HTMLCanvasElement>();
+  const graph = useGraph(graphEl, {
+    settings: {
+      persistentStorageKey: "dijkstras",
+    }
+  });
+</script>
+
+<template>
+  <div class="w-full h-full relative">
+    <Graph
+      @graph-ref="(el) => (graphEl = el)"
+      :graph="graph"
+    />
+  </div>
+</template>
