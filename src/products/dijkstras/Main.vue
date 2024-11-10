edce735a1f656bdb3b1f0e3aba04441cdcd7a2b2
Thomas Eykamp <embergameslp@gmail.com>
2024-11-10 02:35:44 -0500

    add girl theme

--- src/products/dijkstras/Main.vue
+++ src/products/dijkstras/Main.vue
@@ -2,12 +2,14 @@
   import { ref } from "vue";
   import { useGraph } from "@graph/useGraph";
   import Graph from "@graph/Graph.vue";
+import { THEMES } from "@graph/themes";
 
   const graphEl = ref<HTMLCanvasElement>();
   const graph = useGraph(graphEl, {
     settings: {
       persistentStorageKey: "dijkstras",
-    }
+    },
+    theme: THEMES.girl
   });
 </script>
 
