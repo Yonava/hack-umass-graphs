c4603ff2b4a906a565ed605e2a81844010d68efd
yonava <yona@vossandreae.com>
2024-11-10 09:08:11 -0500

    cost display fn

---
+++ src/products/dijkstras/CostDisplay.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import type { GNode, Graph } from '@graph/types';
+
+const props = defineProps<{
+  graph: Graph;
+}>()
+
+const getNodeCosts = (node: GNode) => props.graph.getTheme('nodeText', node);
+</script>
+
+<template>
+  <div v-for="node in graph.nodes.value">
+    <div>
+      {{ node.label }} -
+    </div>
+    <div>
+      {{ getNodeCosts(node) }}
+    </div>
+  </div>
+</template>
